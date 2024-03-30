eda7c038345519598499771ed7a83040efd4c963
Scott Brumley <scott@nsisecure.com>
2024-03-29 23:40:02 -0400

    Add firewall enable clause

--- modules/winsrv-latest-static/files/ConfigureRemotingForAnsible.ps1
+++ modules/winsrv-latest-static/files/ConfigureRemotingForAnsible.ps1
@@ -435,4 +435,5 @@ Else {
 Write-VerboseLog "PS Remoting has been successfully configured for Ansible."
 
 Write-Host "Open Firewall Port"
-netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" enable=yes dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=5985
+netsh advfirewall firewall set rule name="Windows Remote Management (HTTPS-In)" new enabled=yes
