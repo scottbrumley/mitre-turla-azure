35258ca69126fe4719683b84b9ff18c44fa7cccd
scottbrumley <scott@nsisecure.com>
2024-04-10 17:20:24 -0400

    Update bastion.tf
    Adding prefix for bastion host and pip

--- evals-ranges/bastion.tf
+++ evals-ranges/bastion.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "bastion-pip" {
- name                = "bastion-pip"
+ name                = "${var.name-prefix}-pip"
  location            =  module.rgroup.location
  resource_group_name = module.rgroup.name
  allocation_method   = "Static"
@@ -7,7 +7,7 @@ resource "azurerm_public_ip" "bastion-pip" {
 }
 
 resource "azurerm_bastion_host" "bastion-host" {
- name                = "bastion"
+ name                = "${var.name-prefix}-bastion"
  location            = module.rgroup.location
  resource_group_name = module.rgroup.name
  ip_configuration {
