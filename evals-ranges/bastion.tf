dfcf472864468733f54bf64d5df410afdaa613f2
scottbrumley <scott@nsisecure.com>
2024-04-10 20:33:38 -0400

    Update bastion.tf
    Bastion requires it's own virtual network and Subnet specifically named AzureBastionSubnet

--- evals-ranges/bastion.tf
+++ evals-ranges/bastion.tf
@@ -1,5 +1,19 @@
+resource "azurerm_virtual_network" "bastion-virtnet" {
+ name                = "${var.name-prefix}-bastion-vnet"
+ address_space       = ["192.168.1.0/24"]
+ location            = module.rgroup.location
+ resource_group_name = module.rgroup.name
+}
+
+resource "azurerm_subnet" "bastion-subnet" {
+ name                 = "AzureBastionSubnet"
+ resource_group_name  = module.rgroup.name
+ virtual_network_name = azurerm_virtual_network.bastion-virtnet.name
+ address_prefixes     = ["192.168.1.224/27"]
+}
+
 resource "azurerm_public_ip" "bastion-pip" {
- name                = "${var.name-prefix}-pip"
+ name                = "${var.name-prefix}-bastion-pip"
  location            =  module.rgroup.location
  resource_group_name = module.rgroup.name
  allocation_method   = "Static"
@@ -12,7 +26,7 @@ resource "azurerm_bastion_host" "bastion-host" {
  resource_group_name = module.rgroup.name
  ip_configuration {
   name                 = "configuration"
-  subnet_id            = azurerm_subnet.carbon_srv.id
+  subnet_id            = azurerm_subnet.bastion-subnet.id
   public_ip_address_id = azurerm_public_ip.bastion-pip.id
  }
 }
