2b239ba827954a57b0b8be9f461bd42c2092aff5
scottbrumley <scott@nsisecure.com>
2024-05-06 11:07:55 -0400

    Do not destroy Resource Group

--- evals-ranges/main.tf
+++ evals-ranges/main.tf
@@ -22,6 +22,9 @@ module "rgroup" {
   source   = "../modules/rgroup"
   name     = "${var.name-prefix}-rg"
   location = var.location
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 #####################################################################################
